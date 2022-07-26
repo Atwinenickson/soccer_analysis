3ad29dcd73ed11f6c186f9ba1276160c14f72aa1
Atwine Nickson <atwine.nickson@sybyl.com>
2022-07-26 16:18:59 +0300

    get minimum and maximum

--- test.py
+++ test.py
@@ -32,13 +32,52 @@ import sqlite3
 # WHERE t.city = c.CityName
 
 
-# conn = sqlite3.connect('db/database.sqlite')
-# c = conn.cursor()
+conn = sqlite3.connect('db/database.sqlite')
+c = conn.cursor()
 
 # df_players = pd.read_sql(f"Select * from Player p, Player_Attributes pa WHERE p.player_api_id = pa.player_api_id", conn)
 
 # df_players.to_csv('newplayers.csv', index=False)
 
+# df_players = pd.read_sql(f"SELECT * FROM Player LEFT JOIN Player_Attributes ON Player.player_api_id = Player_Attributes.player_api_id WHERE Player.player_api_id != ''", conn)
+# df_players.to_csv('newplayers.csv', index=False)
+
+
 
 df_leaguee = pd.read_csv('newplayers.csv')
-print(df_leaguee.head())
+
+# remove duplicate columns
+df_players_new = df_leaguee.loc[~df_leaguee.index.duplicated(),:].copy()
+# print(df_players_new.head())
+
+# df_players_new['max_crossing'] = df_players_new.groupby('player_name')['crossing'].transform('max')
+# print(df_players_new)
+
+df_players_new = df_players_new.drop_duplicates(subset=['player_name'], keep='first')
+# print(df_players_new)
+
+# MAX CROSSING
+max_crossing = df_players_new['crossing'].max()
+max_finishing = df_players_new['finishing'].max()
+max_heading_accuracy = df_players_new['heading_accuracy'].max()
+max_short_passing = df_players_new['short_passing'].max()
+max_volleys = df_players_new['volleys'].max()
+max_dribbling = df_players_new['dribbling'].max()
+max_curve = df_players_new['curve'].max()
+print(max_crossing)
+
+
+# MIN CROSSING
+min_crossing = df_players_new['crossing'].min()
+min_finishing = df_players_new['finishing'].min()
+min_heading_accuracy = df_players_new['heading_accuracy'].min()
+min_short_passing = df_players_new['short_passing'].min()
+min_volleys = df_players_new['volleys'].min()
+min_dribbling = df_players_new['dribbling'].min()
+min_curve = df_players_new['curve'].min()
+print(min_crossing)
+
+# print(df_leaguee.describe())
+
+# print(df_leaguee['height'].mean())
+# print(df_leaguee['weight'].mean())
