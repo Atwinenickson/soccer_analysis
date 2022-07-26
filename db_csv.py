1d7dfe62bd05f3885220620c1a60355a9fb4cddc
Atwine Nickson <atwine.nickson@sybyl.com>
2022-07-26 11:52:45 +0300

    add git repo

--- db_csv.py
+++ db_csv.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import sqlite3
 
-conn = sqlite3.connect('')
+conn = sqlite3.connect('db/database.sqlite')
 c = conn.cursor()
 
-df = pd.read_sql('SELECT * from wat', conn)
+df = pd.read_sql('SELECT * from Player', conn)
 
 df.to_csv('soccer.csv', index=False)
