15b6d66bbf4ec908434d148647856d965677fa00
Atwine Nickson <atwine.nickson@sybyl.com>
2022-07-26 17:22:59 +0300

    remove unwanted columns

--- test.py
+++ test.py
@@ -5,14 +5,16 @@ import sqlite3
 df_leaguee = pd.read_csv('newplayers.csv')
 
 # remove duplicate columns
-df_players_new = df_leaguee.loc[~df_leaguee.index.duplicated(),:].copy()
+df_players = df_leaguee.loc[~df_leaguee.index.duplicated(),:].copy()
 
-df_players_new = df_players_new.drop_duplicates(subset=['player_name'], keep='first')
-
-
+df_players_new = df_players.drop_duplicates(subset=['player_name'], keep='first')
 
 
-categories = ['crossing', 'finishing']
+
+df_players_categories = df_players_new.drop(df_players.columns[[0, 1, 2, 3, 4,  7, 8, 9, 10,  13, 14, 15]], axis=1)
+
+categories = df_players_categories.columns.values.tolist()
+print(categories)
 
 # Max per category
 def max_rating(category):
@@ -22,6 +24,8 @@ def max_rating(category):
 
 best_rating = lambda : [print(max_rating(category)) for category in categories]
 best_rating()
+
+print('..............................')
 
 
 # Min per category
@@ -33,21 +37,25 @@ def min_rating(category):
 worst_rating = lambda : [print(min_rating(category)) for category in categories]
 worst_rating()
 
+print('..............................')
+
 
 # BEST PLAYER
 def best_player_category(category):
     best_player = df_players_new[df_players_new[category]== df_players_new[category].max()]
-    return best_player
+    return best_player['player_name']
 
 
 best_player = lambda : [print(best_player_category(category)) for category in categories]
 best_player()
 
+print('..............................')
+
 # WORST PLAYER
 def worst_player_category(category):
     worst_player = df_players_new[df_players_new[category]== df_players_new[category].min()]
-    return worst_player
+    return worst_player['player_name']
 
-worst_player = lambda : [print(best_player_category(category)) for category in categories]
+worst_player = lambda : [print(worst_player_category(category)) for category in categories]
 worst_player()
 
